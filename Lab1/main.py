8b392ad6f0e9a9347ed8b9b51b70cc1c02dd72a6
Workstation\Dmytr <filimonov.d@lasertag.net>
2023-11-29 17:04:14 +0200

    save

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,1 +1,13 @@
-import pyspark as sp
+#import pyspark as sp
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf, explode
+from pyspark.sql.types import StringType, ArrayType
+import os,sys
+
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+# Ініціалізація SparkSession
+spark = SparkSession.builder.appName("lab1de").getOrCreate()
+# читаємо файл
+#df_git = spark.read.json("10K.github.jsonl")
