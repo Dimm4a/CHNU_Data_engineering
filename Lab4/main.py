55a1580198504ff62588bfc92bcecccb303551ac
Workstation\Dmytr <filimonov.d@lasertag.net>
2023-12-05 00:27:55 +0200

    feat: Lab4. Написав пошук файлів у каталозі та читання їх вмісту

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,4 +1,9 @@
 import psycopg2
+import glob
+import csv
+
+root_dir = 'data'
+file_type = 'csv'
 
 
 def main():
@@ -8,6 +13,26 @@ def main():
     pas = "postgres"
     conn = psycopg2.connect(host=host, database=database, user=user, password=pas)
     # your code here
+
+    cursor = conn.cursor()
+
+def main():
+
+    file_list = glob.glob(root_dir + '/**/*.' + file_type, recursive=True)
+    print(file_list)
+
+    for filename in file_list:
+        basename = filename.split('.')[0]
+        basename = basename.split('\\')[-1]
+        print(basename)
+
+        with open(filename, "r") as f:
+            csv_reader = list(csv.reader(f))[0]
+            #csv_reader = csv.reader(f)
+            print()
+            print(csv_reader)
+            #for row in csv_reader:
+            #    print(', '.join(row))
 
 
 if __name__ == "__main__":
